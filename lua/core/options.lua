c8f4a06e4acb986dd887e06657d2c482033c84a6
jrafaaael <jrmd070102@gmail.com>
2022-01-13 14:13:31 -0400

    [unused]

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -85,6 +85,18 @@ cmd([[
   :hi illuminatedWord cterm=underline gui=underline
 ]])
 
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] =
+--   vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+--       -- Disable underline, it's very annoying
+--       underline = false,
+--       -- virtual_text = false,
+--       -- Enable virtual text, override spacing to 4
+--       -- virtual_text = {spacing = 4},
+--       -- Use a function to dynamically turn signs off
+--       -- and on, using buffer local variables
+--       signs = true,
+--       update_in_insert = false
+--   })
 
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
