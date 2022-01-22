483a2f8c9358e70931a9ae518bca6caac1a4dea9
jrafaaael <jrmd070102@gmail.com>
2022-01-21 20:02:17 -0400

    change mappings

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -22,10 +22,10 @@ require'nvim-treesitter.configs'.setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = ',',
-      node_incremental = '<S-l>',
+      init_selection = '<',
+      node_incremental = '<',
       scope_incremental = 'grc',
-      node_decremental = '<S-h>',
+      node_decremental = '>',
     },
   },
 }
