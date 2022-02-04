4468c4ad9b581c28c0d7c660440e0c4c26b17852
jrafaaael <jrmd070102@gmail.com>
2022-02-03 22:21:13 -0400

    only show indent in the current context

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,5 +1,9 @@
-require("indent_blankline").setup {
-  buftype_exclude = {"terminal"},
-  filetype_exclude = { "alpha", "packer", "Trouble" },
+require('indent_blankline').setup {
+  buftype_exclude = {'terminal'},
+  filetype_exclude = {'NvimTree', 'alpha', 'packer', 'Trouble'},
+  char = "",
+  context_char = "│",
+  show_current_context = true,
+  show_current_context_start = true,
   use_treesitter = true,
 }
