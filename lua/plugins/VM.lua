922585f4b94d3347a1bb37c91863cf5ce6789806
jrafaaael <jrmd070102@gmail.com>
2022-02-03 22:20:31 -0400

    swap mappings

--- lua/plugins/VM.lua
+++ lua/plugins/VM.lua
@@ -3,6 +3,6 @@ vim.cmd([[
   let g:VM_maps = {}
   let g:VM_maps['Find Under'] = '<C-d>'
   let g:VM_maps['Find Subword Under'] = '<C-d>'
-  let g:VM_maps["Add Cursor Down"] = '<C-j>'
-  let g:VM_maps["Add Cursor Up"] = '<C-k>'
+  let g:VM_maps["Add Cursor Down"] = '<C-Down>'
+  let g:VM_maps["Add Cursor Up"] = '<C-Up>'
 ]])
