05442c3ff0c4dd9a679083e69fbc2c2e59029b64
jrafaaael <jrmd070102@gmail.com>
2022-02-03 22:21:45 -0400

    remove indent markers. add icons config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,12 +1,12 @@
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
 vim.g.nvim_tree_quit_on_open = 1
-vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_indent_markers = 0
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_root_folder_modifier = ':~'
 vim.g.nvim_tree_add_trailing = 1
-vim.g.nvim_tree_group_empty = 1
+vim.g.nvim_tree_group_empty = 0
 vim.g.nvim_tree_disable_window_picker = 0
 vim.g.nvim_tree_icon_padding = ' '
 vim.g.nvim_tree_symlink_arrow = ' >> '
@@ -36,6 +36,12 @@ vim.g.nvim_tree_icons = {
     symlink = "",
     symlink_open = "",
   }
+}
+vim.g.nvim_tree_show_icons = {
+  git = 1,
+  folders = 1,
+  files = 1,
+  folder_arrows = 1,
 }
 
 require'nvim-tree'.setup {
