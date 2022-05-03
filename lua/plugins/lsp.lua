a24c9b9f2cc6047abbf87507d40557e538d96c54
jrafaaael <jrmd070102@gmail.com>
2022-05-03 19:02:53 -0400

    breaking changes in lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,8 @@
 local opts = { noremap=true, silent=true }
 local lsp_installer = require("nvim-lsp-installer")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+local servers = { "sumneko_lua", "html", "cssls", "cssmodules_ls", "jsonls", "pyright", "tailwindcss", "vimls" }
+
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- vim.api.nvim_set_keymap('n', '<space>d', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
@@ -13,6 +15,9 @@ vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', op
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local mappings = function(bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
@@ -32,109 +37,36 @@ local on_attach = function(client, bufnr)
   require"lsp_signature".on_attach({
     hint_enable = false,
   })  -- Note: add in lsp client on-attach
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
   mappings(bufnr)
 end
 
+lsp_installer.setup({
+  ensure_installed = servers
+})
+
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
--- local servers = { 'pyright', 'tsserver' }
--- for _, lsp in pairs(servers) do
---  require('lspconfig')[lsp].setup {
---    on_attach = on_attach,
---    flags = {
---      -- This will be the default in neovim 0.7+
---      debounce_text_changes = 150,
---    }
---  }
--- end
+for _, lsp in pairs(servers) do
+ require('lspconfig')[lsp].setup {
+   on_attach = on_attach,
+   flags = {
+     -- This will be the default in neovim 0.7+
+     debounce_text_changes = 150,
+   },
+   capabilities = capabilities,
+ }
+end
 
-local typescript_settings = {
-  init_options = require("nvim-lsp-ts-utils").init_options,
-  on_attach = function(client, bufnr)
-    local ts_utils = require("nvim-lsp-ts-utils")
-
-    -- disable tsserver formatting if you plan on formatting via null-ls
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
-
-    ts_utils.setup({
-      debug = false,
-      disable_commands = false,
-      enable_import_on_completion = false,
-
-      -- import all
-      import_all_timeout = 5000, -- ms
-      -- lower numbers = higher priority
-      import_all_priorities = {
-        same_file = 1, -- add to existing import statement
-        local_files = 2, -- git files or files with relative path markers
-        buffer_content = 3, -- loaded buffer content
-        buffers = 4, -- loaded buffer names
-      },
-      import_all_scan_buffers = 100,
-      import_all_select_source = false,
-      -- if false will avoid organizing imports
-      always_organize_imports = true,
-
-      -- filter diagnostics
-      filter_out_diagnostics_by_severity = {},
-      filter_out_diagnostics_by_code = {},
-
-      -- inlay hints
-      auto_inlay_hints = false,
-      inlay_hints_highlight = "Comment",
-      inlay_hints_priority = 200, -- priority of the hint extmarks
-      inlay_hints_throttle = 150, -- throttle the inlay hint request
-      inlay_hints_format = { -- format options for individual hint kind
-        Type = {},
-        Parameter = {},
-        Enum = {},
-        -- Example format customization for `Type` kind:
-        -- Type = {
-        --     highlight = "Comment",
-        --     text = function(text)
-        --         return "->" .. text:sub(2)
-        --     end,
-        -- },
-      },
-
-      -- update imports on file move
-      update_imports_on_move = false,
-      require_confirmation_on_move = false,
-      watch_dir = nil,
-    })
-
-    -- required to fix code action ranges and filter diagnostics
-    ts_utils.setup_client(client)
-
-    -- no default maps, so you may want to define some here
-    -- local opts = { silent = true }
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lo", ":TSLspOrganize<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lR", ":TSLspRenameFile<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>li", ":TSLspImportAll<CR>", opts)
-    mappings()
-  end,
-}
-
--- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
--- or if the server is already installed).
-lsp_installer.on_server_ready(function(server)
-  local server_opts = {
+require("typescript").setup({
+  disable_formatting = true,
+  server = {
     on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    },
     capabilities = capabilities,
   }
+})
 
-  -- This setup() function will take the provided server configuration and decorate it with the necessary properties
-  -- before passing it onwards to lspconfig.
-  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-  server:setup(server_opts)
-
-  -- (optional) Customize the options passed to the server
-  if server.name == "tsserver" then
-    server:setup(typescript_settings)
-  end
-end)
