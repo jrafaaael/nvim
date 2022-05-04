47a18500f7f835a226e08e159c1433bc5d8299ee
jrafaaael <jrmd070102@gmail.com>
2022-05-04 10:12:01 -0400

    fix completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -64,7 +64,7 @@ cmp.setup({
     -- { name = 'luasnip' }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
     -- { name = 'snippy' }, -- For snippy users.
-    -- { name = 'buffer' },
+    { name = 'buffer' },
     { name = 'path' },
   }),
 })
