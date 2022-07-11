a1ee2c01ae8094d40d391d8786f59576347fe909
jrafaaael <jrmd070102@gmail.com>
2022-07-11 16:55:10 -0400

    remove padding

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,7 +5,7 @@ wk.setup {
     border = 'single', -- none, single, double, shadow
     position = 'bottom', -- bottom, top
     margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 3, 2, 3, 2 }, -- extra window padding [top, right, bottom, left]
+    padding = { 3, 0, 3, 0 }, -- extra window padding [top, right, bottom, left]
     winblend = 0
   },
   layout = {
