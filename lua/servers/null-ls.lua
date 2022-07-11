39d24e3bd56142c2eec6fe79661a70c17a0b6fe4
jrafaaael <jrmd070102@gmail.com>
2022-07-11 16:56:09 -0400

    only format with null-ls

--- lua/servers/null-ls.lua
+++ lua/servers/null-ls.lua
@@ -1,8 +1,14 @@
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local FORMAT_ON_SAVE = false
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettierd.with({
+      condition = function(utils)
+          return utils.root_has_file({ '.prettierd.js' })
+        end
+    }),
     null_ls.builtins.diagnostics.eslint_d.with({
         condition = function(utils)
           return utils.root_has_file({ '.eslintrc.js' })
@@ -11,4 +17,19 @@ null_ls.setup({
     null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.formatting.black
   },
+  on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+            if FORMAT_ON_SAVE then
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end
+          end,
+        })
+      end
+    end,
 })
