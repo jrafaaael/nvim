f882f2216e2f2d5cbbf988dad1fe2260d3ebaa26
jrafaaael <jrmd070102@gmail.com>
2022-07-11 16:54:46 -0400

    fix weird highlight

---
+++ lua/plugins/searchbox.lua
@@ -0,0 +1,14 @@
+require('searchbox').setup({
+  popup = {
+    -- other popup options
+    border = {
+      -- other popup border options
+      -- do not include `highlight` here
+      highlight = 'Normal:Normal FloatBorder:Normal',
+    },
+    win_options = {
+      winhighlight = 'Normal:Normal',
+    },
+  },
+  -- other options
+})
