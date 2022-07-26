f7e01dad03c58928978a1e3225c14166ab2ea765
jrafaaael <jrmd070102@gmail.com>
2022-07-26 10:06:54 -0400

    format

--- lua/servers/null-ls.lua
+++ lua/servers/null-ls.lua
@@ -19,18 +19,18 @@ null_ls.setup({
     null_ls.builtins.formatting.black
   },
   on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-            if FORMAT_ON_SAVE then
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end
-          end,
-        })
-      end
-    end,
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          if FORMAT_ON_SAVE then
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end
+        end,
+      })
+    end
+  end,
 })
