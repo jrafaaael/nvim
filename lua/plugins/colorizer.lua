7cc8cdd91c2878f9c4645e1ae91dba1d6497c451
jrafaaael <jrmd070102@gmail.com>
2022-09-15 18:44:03 -0400

    default mode

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -5,7 +5,5 @@ colorizer.setup({
     RRGGBBAA = true, -- #RRGGBBAA hex codes
     css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
     tailwind = true, -- Enable tailwind colors
-    -- Available modes for `mode`: foreground, background,  virtualtext
-    mode = "foreground", -- Set the display mode.
   }
 })
