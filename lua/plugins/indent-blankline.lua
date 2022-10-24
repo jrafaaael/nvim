6fa4f2a8b17ec01efb564da6710286d116c5f712
jrafaaael <jrmd070102@gmail.com>
2022-10-24 12:43:47 -0400

    remove space char

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,14 +1,9 @@
 local cmd = vim.cmd
 
-vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-
 require('indent_blankline').setup {
   buftype_exclude = { 'terminal', "prompt", "nofile", "help" },
   filetype_exclude = { 'NvimTree', 'alpha', 'packer', 'Trouble', 'lspinfo' },
   use_treesitter = true,
-  space_char_blankline = " ",
-  show_current_context = true,
 }
 
 cmd 'hi IndentBlanklineChar guifg=#3d4351'
