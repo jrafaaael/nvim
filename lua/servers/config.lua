f04f0a80df6db83924fe3a195d44323b0e93d405
jrafaaael <jrmd070102@gmail.com>
2022-12-04 19:45:16 -0400

    move tsserver setup to `/server/config.lua` file

--- lua/servers/config.lua
+++ lua/servers/config.lua
@@ -1,8 +1,12 @@
-local opts = { noremap = true, silent = true }
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 local lsp = vim.lsp
 local keymap = vim.keymap.set
+local util = require 'vim.lsp.util'
+
 
 local mappings = function(bufnr)
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- vim.api.nvim_set_keymap('n', '<space>d', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
@@ -10,9 +14,9 @@ local mappings = function(bufnr)
   -- vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   -- vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  keymap('n', 'gD', vim.lsp.buf.declaration, opts)
+  keymap('n', 'gd', vim.lsp.buf.definition, opts)
+  keymap('n', 'gi', vim.lsp.buf.implementation, opts)
   -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'KI', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 
@@ -24,7 +28,7 @@ local mappings = function(bufnr)
   -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>.', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-f>', '<cmd>lua vim.lsp.buf.format({async=true})<CR>', opts)
+  keymap('n', '<C-f>', vim.lsp.buf.format, { noremap = true, silent = true })
 
   keymap("n", "<leader>lf", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
   keymap("n", "<leader>ld", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
@@ -37,7 +41,7 @@ local mappings = function(bufnr)
   keymap("n", "KI", "<Cmd>Lspsaga signature_help<CR>", { silent = true })
 end
 
-_G.on_attach = function(client, bufnr)
+local on_attach = function(client, bufnr)
   require "lsp_signature".on_attach({
     hint_enable = false,
   }) -- Note: add in lsp client on-attach
@@ -60,3 +64,33 @@ vim.diagnostic.config({
     border = 'rounded',
   },
 })
+
+local formatting_callback = function(client, bufnr)
+  vim.keymap.set('n', '<leader>f', function()
+    local params = util.make_formatting_params({})
+    client.request('textDocument/formatting', params, nil, bufnr)
+  end, { buffer = bufnr })
+end
+
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+for server, config in pairs(vim.g.SERVERS) do
+  if server == "tsserver" then
+    require("typescript").setup({
+      disable_commands = false, -- prevent the plugin from creating Vim commands
+      debug = false, -- enable debug logging for commands
+      server = {
+        on_attach = function(client, bufnr)
+          on_attach(client, bufnr)
+          formatting_callback(client, bufnr)
+        end,
+        capabilities = capabilities
+      },
+    })
+  else
+    require('lspconfig')[server].setup(
+      vim.tbl_deep_extend("force", { capabilities = capabilities, on_attach = on_attach }, config)
+    )
+  end
+end
