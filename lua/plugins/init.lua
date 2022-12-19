f2b67a08a0fc45e9f87abe158e1cfcdcd3630408
jrafaaael <jrmd070102@gmail.com>
2022-12-18 20:37:16 -0400

    idk

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,19 +7,21 @@ require('plugins.bufferline')
 require('plugins.neodim')
 require('plugins.lspsaga')
 
+-- Intellisense
 require('plugins.cmp')
 
 -- IDE
-require('plugins.toggleterm')
-require('plugins.nvim-treesitter')
 require('plugins.indent-blankline')
 require('plugins.nvim-autopairs')
-require('plugins.nvim-colorizer')
+require('plugins.colorizer')
 require('plugins.nvim-ts-autotag')
 require('plugins.nvim-comment')
 require('plugins.trouble')
-require('plugins.spectre')
+-- require('plugins.spectre')
 require('plugins.dap')
+require('plugins.toggleterm')
+require('plugins.nvim-treesitter')
+require('plugins.searchbox')
 
 -- git
 require('plugins.gitsigns')
